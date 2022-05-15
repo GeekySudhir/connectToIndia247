885e619046dbcd7019bf527620dd853773916785
ashishsah1000 <ashishsah1000@gamil.com>
2022-05-15 22:41:05 +0530

    added the jobs page and basic table

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,17 @@ var router = express.Router();
 router.get("/", function (req, res, next) {
   res.render("index", { title: "connectToIndia247" });
 });
+
+// route for apply form that will carry the data from list of jobs
 router.get("/apply", function (req, res, next) {
   res.render("apply", { title: "connect2India247" });
 });
 
+// route for jobs lists from the database
+
+router.get("/jobs", function (req, res, next) {
+  res.render("jobs", { title: "connect2India247" });
+});
+
+// route for seeing a specific job
 module.exports = router;
