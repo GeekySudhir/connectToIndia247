89718aeda3fdccc43ee570189ff1d1cf92714b55
ashishsah1000 <ashishsah1000@gamil.com>
2022-05-17 21:18:31 +0530

    added mongodb atlas support and working database model

--- config/database.js
+++ config/database.js
@@ -2,11 +2,14 @@ const mongoose = require("mongoose");
 
 const connectDatabase = () => {
   mongoose
-    .connect(process.env.DB_LOCAL_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      // useCreateIndex:true
-    })
+    .connect(
+      "mongodb+srv://flekenstine:ashish1998@cluster0.5wqh9.mongodb.net/?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        // useCreateIndex:true
+      }
+    )
     .then((con) => {
       console.log(
         "mongoDB database connected with HOST : ",
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+const Jobs = require("../models/jobs");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -12,10 +13,36 @@ router.get("/apply", function (req, res, next) {
 });
 
 // route for jobs lists from the database
+router.get("/jobs", async function (req, res, next) {
+  var data = await Jobs.find().limit(10);
+  console.log("from data:", data);
+  res.render("jobs", { title: "connect2India247", data: data });
+});
 
-router.get("/jobs", function (req, res, next) {
+// route for adding a specific job
+router.post("/add/job", async function (req, res, next) {
+  console.log(req.body, "inserting a document");
+  var job = new Jobs({ ...req.body });
+  job.save((err, result) => {
+    if (err) console.log(err);
+    else console.log(result);
+  });
+
   res.render("jobs", { title: "connect2India247" });
 });
+// route for adding a specific job
+router.get("/add/job", function (req, res, next) {
+  res.render("admin/addJobs", { title: "connect2India247" });
+});
+
+// for seeing the existing job
+router.get("/list/job", async function (req, res, next) {
+  var data = await Jobs.find().limit(10);
+  console.log("from data:", data);
+  res.render("admin/listJobs", { title: "connect2India247", data: data });
+});
+
+//for deleting a specific job
 
 // route for seeing a specific job
 module.exports = router;
